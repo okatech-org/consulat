145c0fbed41e218889f46b5ae3be1c33bb9fd5b9
okacoder <admin@okafrancois.dev>
2025-05-20 19:03:27 +0200

    feat: Enhance AuthenticatedLayout and AppSidebar with TeamSwitcher and logo integration
    - Introduced TeamSwitcher component in AuthenticatedLayout for improved team management.
    - Integrated organization logo handling with fallback in AuthenticatedLayout.
    - Refactored AppSidebar to remove TeamSwitcher and logo, streamlining sidebar content.
    - Adjusted layout styles for better responsiveness and user experience.

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -9,6 +9,14 @@ import { NavUser } from '@/components/layouts/nav-user';
 import { BottomNavigation } from '@/components/ui/bottom-navigation';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
+import { env } from '@/lib/env';
+import { TeamSwitcher } from '@/components/layouts/team-switcher';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const logo =
+  env.NEXT_PUBLIC_ORG_LOGO ||
+  'https://qld7pfnhxe.ufs.sh/f/yMD4lMLsSKvzLokCdOyRlrM9oTwuD7FqvYPzsEJWIQGeR1Vx';
 
 export default async function AuthenticatedLayout({
   children,
@@ -28,18 +36,42 @@ export default async function AuthenticatedLayout({
   return (
     <SidebarProvider>
       <AppSidebar user={currentUser} />
-      <SidebarInset className="overflow-hidden w-full flex flex-col relative pb-safe pt-[64px]">
-        <header className="fixed top-0 z-50 w-full h-16 bg-card shrink-0 border-b border-border items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex w-full h-full justify-between items-center px-4">
-            <div className="flex items-center gap-2 w-full">
+      <SidebarInset className="overflow-hidden w-full flex flex-col relative pb-safe pt-16">
+        <header className="w-full fixed left-0 right-0 top-0 px-4 z-50 h-16 bg-card shrink-0 border-b border-border items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex gap-4 w-full h-full justify-between items-center overflow-x-hidden">
+            <div className="min-w-max">
+              <TeamSwitcher
+                teams={[
+                  {
+                    name: <span className="text-sm">Consulat</span>,
+                    logo: (
+                      <Link
+                        href={ROUTES.base}
+                        className="flex items-center gap-2 transition-transform duration-300 hover:scale-105"
+                      >
+                        <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-gradient-to-r from-blue-600/10 to-indigo-600/10 text-white">
+                          <Image
+                            src={logo}
+                            width={60}
+                            height={60}
+                            alt="Consulat.ga"
+                            className="relative h-7 w-7 rounded-md transition-transform duration-500 group-hover:scale-105"
+                          />
+                        </div>
+                      </Link>
+                    ),
+                    plan: <span className="flex items-center gap-2">France</span>,
+                  },
+                ]}
+              />
+            </div>
+            <div className="border-l border-border flex items-center gap-1 w-full max-w-[calc(100%-100px)] min-w-0">
               <Button disabled={isMobile} variant="ghost" className="!p-0" asChild>
                 <SidebarTrigger />
               </Button>
               <BreadcrumbMenu />
             </div>
-            <div className="min-w-max right-0">
-              <NavUser user={currentUser} showFeedback={false} />
-            </div>
+            <NavUser user={currentUser} showFeedback={false} />
           </div>
         </header>
         <div
