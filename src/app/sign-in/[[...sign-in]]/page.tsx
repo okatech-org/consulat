5f0edca52e18cb56b012d61a17c969c0df4c25f3
okafrancois <itoutouberny@gmail.com>
2025-09-19 23:04:34 +0200

    refactor: replace div with PageContainer in LoginPage for improved layout consistency and structure

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -4,6 +4,7 @@ import { getTranslations } from 'next-intl/server';
 import { BetaBanner } from '@/components/ui/beta-banner';
 import { SignIn } from '@clerk/nextjs';
 import { ROUTES } from '@/schemas/routes';
+import { PageContainer } from '@/components/layouts/page-container';
 
 const appLogo = env.NEXT_PUBLIC_ORG_LOGO;
 
@@ -11,9 +12,9 @@ export default async function LoginPage() {
   const t = await getTranslations('auth.login');
 
   return (
-    <div className="w-dvw bg-background h-dvh pt-8 p-6 md:pt-6 min-h-max overflow-x-hidden md:overflow-hidden flex items-center justify-center md:grid md:grid-cols-2">
-      <div className="w-full h-full min-h-max overflow-y-auto flex flex-col items-center justify-center">
-        <div className="max-w-lg p-2 space-y-8">
+    <PageContainer className="w-dvw bg-background h-dvh pt-8 p-6 md:pt-6 min-h-max overflow-x-hidden md:overflow-hidden flex items-center justify-center md:grid md:grid-cols-2">
+      <div className="w-full h-full min-h-max flex flex-col items-center justify-center">
+        <div className="flex  overflow-hidden flex-col items-center justify-center p-2 space-y-4">
           <header className="w-full border-b border-border pb-2">
             <div className="flex mb-4 h-max w-max items-center justify-center rounded-lg bg-gradient-to-r from-blue-600/10 to-indigo-600/10 text-white">
               <Image
@@ -24,10 +25,10 @@ export default async function LoginPage() {
                 className="relative h-20 w-20 rounded-md transition-transform duration-500 group-hover:scale-105"
               />
             </div>
-            <h1 className="text-2xl mb-2 font-bold">{t('page.title')}</h1>
-            <p className="text-lg text-muted-foreground">
+            <h1 className="text-xl mb-2 font-bold">
               {t('page.welcome_message', { appName: env.NEXT_PUBLIC_APP_NAME })}
-            </p>
+            </h1>
+            <p className="text-lg text-muted-foreground"></p>
           </header>
           <div className="w-full flex justify-center items-center">
             <SignIn fallbackRedirectUrl={ROUTES.user.base} />
@@ -44,6 +45,6 @@ export default async function LoginPage() {
           height={800}
         />
       </div>
-    </div>
+    </PageContainer>
   );
 }
