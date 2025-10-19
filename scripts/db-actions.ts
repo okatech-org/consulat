d68fef01c30626ebd2863739f74a58899ff3127e
okafrancois <itoutouberny@gmail.com>
2025-10-19 20:32:41 +0200

    refactor: simplify db-actions.ts by removing unused request handling logic and replacing it with a basic console log for improved clarity

--- scripts/db-actions.ts
+++ scripts/db-actions.ts
@@ -1,24 +1,5 @@
-import { api } from '@/convex/_generated/api';
-import type { Id } from '@/convex/_generated/dataModel';
-import { ConvexHttpClient } from 'convex/browser';
-
 async function main() {
-  const convex = new ConvexHttpClient('https://dapper-guineapig-547.convex.cloud');
-
-  const requests = await convex.query(api.functions.request.getAllRequests, {
-    serviceId: 'k57besbz7c6vgc2tfdvgbejnvh7sfn60' as Id<'services'>,
-  });
-
-  await Promise.all(
-    requests.map(async (request) => {
-      console.log('Updating profile', request.profileId);
-      await convex.mutation(api.functions.profile.updateProfile, {
-        profileId: request.profileId as Id<'profiles'>,
-        registrationRequest: request._id,
-      });
-      console.log('Profile updated', request.profileId);
-    }),
-  );
+  console.log('Hello, world!');
 }
 
 void main();
